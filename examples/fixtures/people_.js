dd20cdc8b49382a7a56a19ac903a33dc3af4f81b
Carmine DiMascio <cdimascio@gmail.com>
2019-11-25 23:10:28 -0500

    Update people_.js

--- examples/fixtures/people_.js
+++ examples/fixtures/people_.js
@@ -1,3 +1,5 @@
 module.exports = function(collection) {
+  // create an index (or do other work)
+  // Be sure to return a Promise
   return collection.createIndex( { "address.city": 1 }, { unique: false } );
 }
